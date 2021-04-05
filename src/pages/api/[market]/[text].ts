238e77ffd7e76f93790514a0416e64c87194565d
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-05 14:34:36 +0530

    Move _lib outside of api

--- src/pages/api/[market]/[text].ts
+++ src/pages/api/[market]/[text].ts
@@ -1,8 +1,8 @@
 import { getMarketSpecificsByMarketNameInURLRepresentation } from 'store/markets'
-import { FileType } from '../_lib/types'
+import { FileType } from '../../../_lib/types'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getScreenshot } from '../_lib/chromium'
-import { getOGImageHTML } from '../_lib/getOGImageHTML'
+import { getScreenshot } from '../../../_lib/chromium'
+import { getOGImageHTML } from '../../../_lib/getOGImageHTML'
 
 const isDev = !process.env.AWS_REGION
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1'
