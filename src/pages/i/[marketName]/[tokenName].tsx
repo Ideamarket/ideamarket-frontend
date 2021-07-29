39505a6d469def654564a427cc304bc6deaff77e
Shmoji <jackj_skate@hotmail.com>
2021-07-29 16:39:32 -0400

    Revert "chore: testing with more props filled out"
    This reverts commit 2676c9641732eaf50949be6bde6029fecb2fad97.

--- src/pages/i/[marketName]/[tokenName].tsx
+++ src/pages/i/[marketName]/[tokenName].tsx
@@ -1,5 +1,6 @@
 import { DefaultLayout } from 'components'
 import { NextSeo } from 'next-seo'
+import { getURL } from 'utils/seo-constants'
 import { GetServerSideProps } from 'next'
 
 export default function TokenDetails({
@@ -14,20 +15,13 @@ export default function TokenDetails({
   return (
     <>
       <NextSeo
-        title="test title"
-        description="test description"
+        title="testing"
         openGraph={{
-          type: 'website',
-          url: `testurl`,
-          title: 'default title',
           images: [
             {
               url: `${
-                process.env.NEXT_PUBLIC_OG_IMAGE_URL
-                  ? process.env.NEXT_PUBLIC_OG_IMAGE_URL
-                  : 'https://og-image.ideamarket.io'
-              }/api/twitter/elonmusk.png`,
-              alt: 'twitter:image',
+                process.env.NEXT_PUBLIC_OG_IMAGE_URL ?? getURL()
+              }/api/${rawMarketName}/${rawTokenName}.png`,
             },
           ],
         }}
