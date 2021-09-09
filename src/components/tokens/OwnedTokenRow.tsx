f5c6c2a506db1a50989dd907f4eb52fce34fc988
Shmoji <jackj_skate@hotmail.com>
2021-09-09 17:08:07 -0400

    refactor(WIP): made profile tables use infinite scroll

--- src/components/tokens/OwnedTokenRow.tsx
+++ src/components/tokens/OwnedTokenRow.tsx
@@ -27,12 +27,14 @@ export default function OwnedTokenRow({
   balance,
   balanceBN,
   refetch,
+  lastElementRef,
 }: {
   token: IdeaToken
   market: IdeaMarket
   balance: string
   balanceBN: BN
   refetch: () => void
+  lastElementRef?: (node) => void
 }) {
   const router = useRouter()
   const marketSpecifics = getMarketSpecificsByMarketName(market.name)
@@ -62,180 +64,179 @@ export default function OwnedTokenRow({
   )
 
   return (
-    <>
-      <tr
-        className="grid grid-cols-3 border-b cursor-pointer md:table-row hover:bg-brand-gray border-brand-border-gray dark:hover:bg-gray-600 dark:border-gray-500"
-        onClick={() => {
-          router.push(
-            `/i/${marketSpecifics.getMarketNameURLRepresentation()}/${marketSpecifics.getTokenNameURLRepresentation(
-              token.name
-            )}`
-          )
-        }}
-      >
-        {/* Market desktop */}
-        <td className="flex items-center justify-center hidden py-4 text-sm leading-5 text-center text-gray-500 dark:text-gray-300 md:table-cell whitespace-nowrap">
-          <div className="flex items-center justify-end w-full h-full">
-            <div className="w-5 h-auto">
-              {marketSpecifics.getMarketSVGTheme(resolvedTheme)}
-            </div>
-          </div>
-        </td>
-        <td className="col-span-3 px-6 py-4 whitespace-nowrap">
-          <div className="flex items-center text-gray-900 dark:text-gray-200">
-            <div className="flex-shrink-0 w-7.5 h-7.5">
-              {isTokenIconLoading ? (
-                <div className="w-full h-full bg-gray-400 rounded-full dark:bg-gray-600 animate-pulse"></div>
-              ) : (
-                <div className="relative w-full h-full rounded-full">
-                  <Image
-                    src={tokenIconURL || '/gray.svg'}
-                    alt="token"
-                    layout="fill"
-                    objectFit="contain"
-                    className="rounded-full"
-                  />
-                </div>
-              )}
-            </div>
-            <div className="ml-4 text-base leading-5">
-              <A
-                href={`${marketSpecifics.getTokenURL(token.name)}`}
-                className="hover:underline"
-                onClick={(e) => {
-                  e.stopPropagation()
-                }}
-              >
-                {token.name}
-              </A>
-            </div>
-            {/* Verified Badge */}
-            {token.tokenOwner !== ZERO_ADDRESS && (
-              <div className="w-5 h-5 ml-1.5">
-                <BadgeCheckIcon />
-              </div>
-            )}
-            <div className="flex items-center justify-center ml-auto md:hidden">
-              <svg
-                className="w-7.5 text-brand-blue dark:text-blue-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-          </div>
-        </td>
-        {/* Market mobile */}
-        <td className="px-6 py-4 whitespace-nowrap md:hidden">
-          <p className="text-sm font-semibold tracking-tightest text-brand-gray-4 dark:text-gray-400">
-            Market
-          </p>
-          <div className="inline-block w-4 h-4">
+    <tr
+      ref={lastElementRef}
+      className="grid grid-cols-3 border-b cursor-pointer md:table-row hover:bg-brand-gray border-brand-border-gray dark:hover:bg-gray-600 dark:border-gray-500"
+      onClick={() => {
+        router.push(
+          `/i/${marketSpecifics.getMarketNameURLRepresentation()}/${marketSpecifics.getTokenNameURLRepresentation(
+            token.name
+          )}`
+        )
+      }}
+    >
+      {/* Market desktop */}
+      <td className="flex items-center justify-center hidden py-4 text-sm leading-5 text-center text-gray-500 dark:text-gray-300 md:table-cell whitespace-nowrap">
+        <div className="flex items-center justify-end w-full h-full">
+          <div className="w-5 h-auto">
             {marketSpecifics.getMarketSVGTheme(resolvedTheme)}
           </div>
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
-            Price
-          </p>
-          <p
-            className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
-            title={'$' + tokenPrice}
-          >
-            $
-            {formatNumberWithCommasAsThousandsSerperator(
-              parseFloat(tokenPrice).toFixed(2)
+        </div>
+      </td>
+      <td className="col-span-3 px-6 py-4 whitespace-nowrap">
+        <div className="flex items-center text-gray-900 dark:text-gray-200">
+          <div className="flex-shrink-0 w-7.5 h-7.5">
+            {isTokenIconLoading ? (
+              <div className="w-full h-full bg-gray-400 rounded-full dark:bg-gray-600 animate-pulse"></div>
+            ) : (
+              <div className="relative w-full h-full rounded-full">
+                <Image
+                  src={tokenIconURL || '/gray.svg'}
+                  alt="token"
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-full"
+                />
+              </div>
             )}
-          </p>
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
-            Balance
-          </p>
-          <p
-            className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
-            title={balance}
-          >
-            {formatNumberWithCommasAsThousandsSerperator(
-              parseFloat(balance).toFixed(2)
-            )}
-          </p>
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
-            Value
-          </p>
-          <p
-            className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
-            title={'$' + balanceValue}
-          >
-            ${balanceValue}
-          </p>
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
-            24H Change
-          </p>
-          <p
-            className={classNames(
-              'text-base leading-4 tracking-tightest-2 uppercase',
-              {
-                'text-brand-red dark:text-red-400':
-                  parseFloat(token.dayChange) < 0.0,
-                'text-brand-green dark:text-green-400':
-                  parseFloat(token.dayChange) > 0.0,
-                'text-very-dark-blue dark:text-gray-300':
-                  parseFloat(token.dayChange) === 0.0,
-              }
-            )}
-            title={
-              parseFloat(token.dayChange) >= 0.0
-                ? `+ ${token.dayChange}%`
-                : `- ${token.dayChange.slice(1)}%`
-            }
-          >
-            {parseFloat(token.dayChange) >= 0.0
-              ? `+ ${formatNumber(token.dayChange)}`
-              : `- ${formatNumber(token.dayChange.slice(1))}`}
-            %
-          </p>
-        </td>
-        {/* Lock Button */}
-        <td className="px-4 py-4 md:px-2 whitespace-nowrap">
-          <button
-            onClick={(e) => {
-              e.stopPropagation()
-              ModalService.open(LockModal, { token, balance, refetch })
-            }}
-            className="w-20 h-10 text-base font-medium text-white border-2 rounded-lg bg-brand-blue dark:bg-gray-600 border-brand-blue dark:text-gray-300 tracking-tightest-2 font-sf-compact-medium"
-          >
-            <span>Lock</span>
-          </button>
-        </td>
-        <td className="px-4 py-4 md:px-2 whitespace-nowrap">
-          <button
-            onClick={(e) => {
-              e.stopPropagation()
-              ModalService.open(GiftModal, { token, balance, refetch })
-            }}
-            className="w-20 h-10 text-base font-medium bg-white border-2 rounded-lg text-brand-blue dark:bg-gray-600 border-brand-blue dark:text-gray-300 tracking-tightest-2 font-sf-compact-medium"
-          >
-            <span>Gift</span>
-          </button>
-        </td>
-        {/* Add to Metamask button */}
-        <td className="px-4 py-4">
-          <div className="flex items-center w-full h-full">
-            <AddToMetamaskButton token={token} />
           </div>
-        </td>
-      </tr>
-    </>
+          <div className="ml-4 text-base leading-5">
+            <A
+              href={`${marketSpecifics.getTokenURL(token.name)}`}
+              className="hover:underline"
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+            >
+              {token.name}
+            </A>
+          </div>
+          {/* Verified Badge */}
+          {token.tokenOwner !== ZERO_ADDRESS && (
+            <div className="w-5 h-5 ml-1.5">
+              <BadgeCheckIcon />
+            </div>
+          )}
+          <div className="flex items-center justify-center ml-auto md:hidden">
+            <svg
+              className="w-7.5 text-brand-blue dark:text-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+      </td>
+      {/* Market mobile */}
+      <td className="px-6 py-4 whitespace-nowrap md:hidden">
+        <p className="text-sm font-semibold tracking-tightest text-brand-gray-4 dark:text-gray-400">
+          Market
+        </p>
+        <div className="inline-block w-4 h-4">
+          {marketSpecifics.getMarketSVGTheme(resolvedTheme)}
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
+          Price
+        </p>
+        <p
+          className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
+          title={'$' + tokenPrice}
+        >
+          $
+          {formatNumberWithCommasAsThousandsSerperator(
+            parseFloat(tokenPrice).toFixed(2)
+          )}
+        </p>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
+          Balance
+        </p>
+        <p
+          className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
+          title={balance}
+        >
+          {formatNumberWithCommasAsThousandsSerperator(
+            parseFloat(balance).toFixed(2)
+          )}
+        </p>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
+          Value
+        </p>
+        <p
+          className="text-base leading-4 uppercase tracking-tightest-2 text-very-dark-blue dark:text-gray-300"
+          title={'$' + balanceValue}
+        >
+          ${balanceValue}
+        </p>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <p className="text-sm font-semibold md:hidden tracking-tightest text-brand-gray-4 dark:text-gray-400">
+          24H Change
+        </p>
+        <p
+          className={classNames(
+            'text-base leading-4 tracking-tightest-2 uppercase',
+            {
+              'text-brand-red dark:text-red-400':
+                parseFloat(token.dayChange) < 0.0,
+              'text-brand-green dark:text-green-400':
+                parseFloat(token.dayChange) > 0.0,
+              'text-very-dark-blue dark:text-gray-300':
+                parseFloat(token.dayChange) === 0.0,
+            }
+          )}
+          title={
+            parseFloat(token.dayChange) >= 0.0
+              ? `+ ${token.dayChange}%`
+              : `- ${token.dayChange.slice(1)}%`
+          }
+        >
+          {parseFloat(token.dayChange) >= 0.0
+            ? `+ ${formatNumber(token.dayChange)}`
+            : `- ${formatNumber(token.dayChange.slice(1))}`}
+          %
+        </p>
+      </td>
+      {/* Lock Button */}
+      <td className="px-4 py-4 md:px-2 whitespace-nowrap">
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+            ModalService.open(LockModal, { token, balance, refetch })
+          }}
+          className="w-20 h-10 text-base font-medium text-white border-2 rounded-lg bg-brand-blue dark:bg-gray-600 border-brand-blue dark:text-gray-300 tracking-tightest-2 font-sf-compact-medium"
+        >
+          <span>Lock</span>
+        </button>
+      </td>
+      <td className="px-4 py-4 md:px-2 whitespace-nowrap">
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+            ModalService.open(GiftModal, { token, balance, refetch })
+          }}
+          className="w-20 h-10 text-base font-medium bg-white border-2 rounded-lg text-brand-blue dark:bg-gray-600 border-brand-blue dark:text-gray-300 tracking-tightest-2 font-sf-compact-medium"
+        >
+          <span>Gift</span>
+        </button>
+      </td>
+      {/* Add to Metamask button */}
+      <td className="px-4 py-4">
+        <div className="flex items-center w-full h-full">
+          <AddToMetamaskButton token={token} />
+        </div>
+      </td>
+    </tr>
   )
 }
--- src/store/ideaMarketsStore.tsx
+++ src/store/ideaMarketsStore.tsx
@@ -169,7 +169,7 @@ export async function queryMarket(
 export async function queryOwnedTokensMaybeMarket(
   queryKey: string,
   market: IdeaMarket,
-  owner: string
+  owner: string,
 ): Promise<IdeaTokenMarketPair[]> {
   if (owner === undefined) {
     return []
@@ -177,7 +177,10 @@ export async function queryOwnedTokensMaybeMarket(
 
   const result = await request(
     HTTP_GRAPHQL_ENDPOINT,
-    getQueryOwnedTokensMaybeMarket(market ? market.marketID : undefined, owner)
+    getQueryOwnedTokensMaybeMarket(
+      market ? market.marketID : undefined,
+      owner,
+    )
   )
 
   return result.ideaTokenBalances.map(
@@ -196,7 +199,7 @@ export async function queryOwnedTokensMaybeMarket(
 export async function queryMyTokensMaybeMarket(
   queryKey: string,
   market: IdeaMarket,
-  owner: string
+  owner: string,
 ): Promise<IdeaTokenMarketPair[]> {
   if (owner === undefined) {
     return []
@@ -591,7 +594,7 @@ export async function queryLockedAmounts(
 export async function queryLockedTokens(
   queryKey,
   market: IdeaMarket,
-  ownerAddress: string
+  ownerAddress: string,
 ): Promise<LockedIdeaTokenMarketPair[]> {
   if (!ownerAddress) {
     return []
@@ -642,7 +645,7 @@ export async function queryLockedTokens(
 export async function queryMyTrades(
   queryKey,
   market: IdeaMarket,
-  ownerAddress: string
+  ownerAddress: string,
 ): Promise<IdeaTokenTrade[]> {
   if (!ownerAddress) {
     return []
