ee60cbed0f9774bbcc0f20836f5f68a8030b3b29
Shmoji <jackj_skate@hotmail.com>
2021-09-30 15:53:07 -0400

    feat: connected merkleDistributor smart contracts to frontend for rinkeby

--- src/store/contractStore.ts
+++ src/store/contractStore.ts
@@ -13,6 +13,7 @@ type State = {
   multiActionContract: any
   uniswapFactoryContract: any
   ideaTokenVaultContract: any
+  merkleDistributorContract: any
 }
 
 export const useContractStore = create<State>((set) => ({
@@ -22,6 +23,7 @@ export const useContractStore = create<State>((set) => ({
   multiActionContract: undefined,
   uniswapFactoryContract: undefined,
   ideaTokenVaultContract: undefined,
+  merkleDistributorContract: undefined,
 }))
 
 export function clearContracts() {
@@ -32,6 +34,7 @@ export function clearContracts() {
     multiActionContract: undefined,
     uniswapFactoryContract: undefined,
     ideaTokenVaultContract: undefined,
+    merkleDistributorContract: undefined,
   })
 }
 
@@ -77,6 +80,12 @@ export function initContractsFromWeb3(web3: Web3) {
     { from: web3.eth.defaultAccount }
   )
 
+  const merkleDistributorContract = new web3.eth.Contract(
+    abis.merkleDistributor as any,
+    deployedAddresses.merkleDistributor,
+    { from: web3.eth.defaultAccount }
+  )
+
   useContractStore.setState({
     daiContract: daiContract,
     factoryContract: factoryContract,
@@ -84,6 +93,7 @@ export function initContractsFromWeb3(web3: Web3) {
     multiActionContract: multiActionContract,
     uniswapFactoryContract: uniswapFactoryContract,
     ideaTokenVaultContract: ideaTokenVaultContract,
+    merkleDistributorContract: merkleDistributorContract,
   })
 }
 
