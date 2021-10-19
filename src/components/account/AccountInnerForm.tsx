ec8b2e0c1912e5590fb9c66fef763913e229894d
Tuan Phung <tuan@phung.cz>
2021-10-19 15:26:38 +0200

    fix: added imo claim validation

--- src/components/account/AccountInnerForm.tsx
+++ src/components/account/AccountInnerForm.tsx
@@ -142,7 +142,9 @@ const AccountInnerForm = ({
           <ProfileWallet walletState="signedIn" userData={getValues()} />
         )}
       </div>
-      <Footer />
+      <div className="text-brand-gray-2">
+        <Footer />
+      </div>
     </div>
   )
 }
