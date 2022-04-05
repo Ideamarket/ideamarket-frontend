9a59247dd679b8aaa7b05d2c9ff4c756c713c667
Shmoji <jackj_skate@hotmail.com>
2022-04-05 15:08:13 -0400

    fix: fixed listing content heights

--- src/components/tokens/OverviewTokenRow.tsx
+++ src/components/tokens/OverviewTokenRow.tsx
@@ -231,61 +231,88 @@ export default function TokenRow({
         </div>
 
         <div className="relative flex items-center w-3/4 mx-auto md:w-full text-gray-900 dark:text-gray-200">
-          {showMarketSVG && marketSpecifics.getMarketSVGTheme(resolvedTheme)}
-          <div
-            className={classNames(
-              'flex-shrink-0 w-7.5 h-7.5',
-              showMarketSVG && 'ml-2'
-            )}
-          >
-            {isTokenIconLoading ? (
-              <div className="w-full h-full bg-gray-400 rounded-full animate-pulse"></div>
-            ) : !isOnChain || market?.name === 'URL' ? (
-              <GlobeAltIcon className="w-7" />
-            ) : (
-              <div className="relative w-full h-full rounded-full">
-                <Image
-                  src={tokenIconURL || '/gray.svg'}
-                  alt="token"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-full"
-                />
+          {/* If tweet, replace meta data preview with Listed By */}
+          {getListingTypeFromIDTURL(token?.url) === LISTING_TYPE.TWEET ? (
+            <>
+              {onchainListedBy && timeAfterOnChainListedInDays && (
+                <div className="px-2 py-2 mt-1 bg-black/[.05] rounded-lg whitespace-nowrap">
+                  Listed by{' '}
+                  {isOnchainListedByETHAddress ? (
+                    <A
+                      className="underline font-bold hover:text-blue-600"
+                      href={`https://arbiscan.io/address/${onchainListedBy}`}
+                    >
+                      {convertAccountName(onchainListedBy)}
+                    </A>
+                  ) : (
+                    <span className="font-bold">
+                      {convertAccountName(onchainListedBy)}
+                    </span>
+                  )}{' '}
+                  {timeAfterOnChainListedInDays} days ago
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              {showMarketSVG &&
+                marketSpecifics.getMarketSVGTheme(resolvedTheme)}
+              <div
+                className={classNames(
+                  'flex-shrink-0 w-7.5 h-7.5',
+                  showMarketSVG && 'ml-2'
+                )}
+              >
+                {isTokenIconLoading ? (
+                  <div className="w-full h-full bg-gray-400 rounded-full animate-pulse"></div>
+                ) : !isOnChain || market?.name === 'URL' ? (
+                  <GlobeAltIcon className="w-7" />
+                ) : (
+                  <div className="relative w-full h-full rounded-full">
+                    <Image
+                      src={tokenIconURL || '/gray.svg'}
+                      alt="token"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-full"
+                    />
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div className="ml-4 text-base font-medium leading-5 truncate z-30">
-            {displayName && (
-              <div>
+              <div className="ml-4 text-base font-medium leading-5 truncate z-30">
+                {displayName && (
+                  <div>
+                    <a
+                      href={`/i/${token?.listingId}`}
+                      onClick={(event) => event.stopPropagation()}
+                      className="text-xs md:text-base font-bold hover:underline"
+                    >
+                      {displayName?.substr(
+                        0,
+                        displayName?.length > 50 ? 50 : displayName?.length
+                      ) + (displayName?.length > 50 ? '...' : '')}
+                    </a>
+                  </div>
+                )}
                 <a
-                  href={`/i/${token?.listingId}`}
-                  onClick={(event) => event.stopPropagation()}
-                  className="text-xs md:text-base font-bold hover:underline"
+                  href={token?.url}
+                  className="text-xs md:text-sm text-brand-blue hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  {displayName?.substr(
+                  {token?.url.substr(
                     0,
-                    displayName?.length > 50 ? 50 : displayName?.length
-                  ) + (displayName?.length > 50 ? '...' : '')}
+                    token?.url.length > 50 ? 50 : token?.url.length
+                  ) + (token?.url.length > 50 ? '...' : '')}
                 </a>
               </div>
-            )}
-            <a
-              href={token?.url}
-              className="text-xs md:text-sm text-brand-blue hover:underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {token?.url.substr(
-                0,
-                token?.url.length > 50 ? 50 : token?.url.length
-              ) + (token?.url.length > 50 ? '...' : '')}
-            </a>
-          </div>
-          {/* Desktop Verified Badge */}
-          {isOnChain && market?.name !== 'URL' && token?.verified && (
-            <div className="hidden md:inline w-5 h-5 ml-1.5 text-black dark:text-white">
-              <IdeaverifyIconBlue className="w-full h-full" />
-            </div>
+              {/* Desktop Verified Badge */}
+              {isOnChain && market?.name !== 'URL' && token?.verified && (
+                <div className="hidden md:inline w-5 h-5 ml-1.5 text-black dark:text-white">
+                  <IdeaverifyIconBlue className="w-full h-full" />
+                </div>
+              )}
+            </>
           )}
         </div>
 
@@ -310,27 +337,29 @@ export default function TokenRow({
                   {timeAfterGhostListedInDays} days ago
                 </div>
               )}
-              {onchainListedBy && timeAfterOnChainListedInDays && (
-                <div className="px-2 py-2 bg-black/[.05] rounded-lg whitespace-nowrap">
-                  Listed by{' '}
-                  {isOnchainListedByETHAddress ? (
-                    <A
-                      className="underline font-bold hover:text-blue-600"
-                      href={`https://arbiscan.io/address/${onchainListedBy}`}
-                    >
-                      {convertAccountName(onchainListedBy)}
-                    </A>
-                  ) : (
-                    <span className="font-bold">
-                      {convertAccountName(onchainListedBy)}
-                    </span>
-                  )}{' '}
-                  {timeAfterOnChainListedInDays} days ago
-                </div>
-              )}
+              {getListingTypeFromIDTURL(token?.url) !== LISTING_TYPE.TWEET &&
+                onchainListedBy &&
+                timeAfterOnChainListedInDays && (
+                  <div className="px-2 py-2 bg-black/[.05] rounded-lg whitespace-nowrap">
+                    Listed by{' '}
+                    {isOnchainListedByETHAddress ? (
+                      <A
+                        className="underline font-bold hover:text-blue-600"
+                        href={`https://arbiscan.io/address/${onchainListedBy}`}
+                      >
+                        {convertAccountName(onchainListedBy)}
+                      </A>
+                    ) : (
+                      <span className="font-bold">
+                        {convertAccountName(onchainListedBy)}
+                      </span>
+                    )}{' '}
+                    {timeAfterOnChainListedInDays} days ago
+                  </div>
+                )}
             </div>
 
-            <div className="px-4 md:px-0">
+            <div className="px-4 md:px-0 md:w-[80%]">
               <ListingContent
                 ideaToken={token}
                 page="HomePage"
