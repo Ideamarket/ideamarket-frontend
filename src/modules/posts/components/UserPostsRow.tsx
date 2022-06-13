4b5d9cae09ecebbdcf398eb2bd53aa0c0a8acd76
Shmoji <jackj_skate@hotmail.com>
2022-06-13 15:19:09 -0400

    fix: re-enabled new posts that are tweet URLs

--- src/modules/posts/components/UserPostsRow.tsx
+++ src/modules/posts/components/UserPostsRow.tsx
@@ -109,7 +109,7 @@ export default function UserPostsRow({
                 )}
 
                 <ListingContent
-                  ideaToken={token}
+                  imPost={token}
                   page="HomePage"
                   urlMetaData={urlMetaData}
                   useMetaData={
@@ -242,7 +242,7 @@ export default function UserPostsRow({
           )}
 
           <ListingContent
-            ideaToken={token}
+            imPost={token}
             page="HomePage"
             urlMetaData={urlMetaData}
             useMetaData={
